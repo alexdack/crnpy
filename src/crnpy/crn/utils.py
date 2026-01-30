104bbb3ca4a3ec271e10834c22738c7271adbd0d
alexdack <alex.dack.96@gmail.com>
2026-01-30 14:47:24 +0000

    Add requirements.txt file

--- src/crnpy/crn/utils.py
+++ src/crnpy/crn/utils.py
@@ -10,3 +10,9 @@ def open_csv(file):
             out_data.append([float(datapoint) for datapoint in row[0].split(',')])
     out_data = np.asarray(out_data) 
     return out_data
+
+def save_crn(filename, txt):
+    # Function to save CRN.txt files
+    f = open(filename, "w")
+    f.write(txt)
+    f.close()
--- tests/test_crn_utils.py
+++ tests/test_crn_utils.py
@@ -3,9 +3,6 @@ import numpy as np
 from pathlib import Path
 import pytest
 
-TEST_DIR = Path(__file__).parent
-DATA_DIR = TEST_DIR / "data"
-
 @pytest.fixture
 def csv_path():
     TEST_DIR = Path(__file__).parent
@@ -13,5 +10,15 @@ def csv_path():
     csv_path = DATA_DIR / "test_1.csv"
     return csv_path
 
+@pytest.fixture
+def crn_to_save():
+    TEST_DIR = Path(__file__).parent
+    DATA_DIR = TEST_DIR / "data"
+    csv_path = DATA_DIR / "crn_to_save.txt"
+    return csv_path
+
 def test_open_csv(csv_path):
     np.testing.assert_array_equal(utils.open_csv(csv_path), np.array([[1., 1.]]))
+
+def test_save_crn(crn_to_save):
+    assert True
