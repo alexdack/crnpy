13443875fd6a3611ccce7a8bddc4d40331b02e9a
alexdack <alex.dack.96@gmail.com>
2026-01-30 16:00:54 +0000

    Add read CRN with sorting + test

--- src/crnpy/crn/utils.py
+++ src/crnpy/crn/utils.py
@@ -16,3 +16,73 @@ def save_crn(filename, txt):
     f = open(filename, "w")
     f.write(txt)
     f.close()
+
+def read_crn_txt(filename):
+    # Function to parse CRN.txt files to use in python 
+    f = open(filename, "r")
+    species_and_nothing = {''}
+    reaction_rates = []
+    reaction_reacts = []
+    reaction_prods = []
+    initial_concs = {}
+    for x in f:
+        if x.startswith("#"):
+            conc_string = x.replace(' ', '').strip(' \n\t#')
+            split_concs = conc_string.split(",")
+            for conc in split_concs:
+                species_name, conc_val = conc.split("=")
+                if species_name in initial_concs:
+                    conc_val_stored = initial_concs[species_name]
+                    max_conc = max(conc_val_stored, float(conc_val))
+                    initial_concs[species_name]= float(max_conc)
+                else:
+                    initial_concs.update({species_name: float(conc_val)})
+        else:
+            x = x.replace(' ', '').strip(' \n\t')
+            split_comma = x.split(",")
+            rate = float(split_comma[1].strip(' \n\t'))
+            split_arrow = split_comma[0].split("->")
+            reactants = split_arrow[0].split("+")
+            products = split_arrow[1].split("+")
+
+            reaction_rates = reaction_rates + [rate]
+
+            for reactant in reactants:
+                species_and_nothing.add(reactant)
+
+            for product in products:
+                species_and_nothing.add(product)
+
+            reaction_reacts = reaction_reacts + [reactants]
+            reaction_prods = reaction_prods + [products]
+    
+    number_species = len(species_and_nothing) - 1 
+    species = list(species_and_nothing)[1:]
+    number_reactions = len(reaction_rates)
+    
+    react_stoch = np.zeros(shape=(number_reactions, number_species))
+    prod_stoch = np.zeros(shape=(number_reactions, number_species))
+
+    for r in np.arange(0, number_reactions):
+        reacts_for_react = reaction_reacts[r]
+        prods_for_react = reaction_prods[r]
+        for s in np.arange(0, number_species):
+            react_stoch[r,s] = reacts_for_react.count(species[s])
+            prod_stoch[r,s] = prods_for_react.count(species[s])
+
+    stoch_mat = prod_stoch - react_stoch
+    
+    initial_concs_vec = np.zeros(shape=(number_species,))
+    for species_idx in np.arange(0,len(species)):
+        if species[species_idx] in initial_concs:
+            initial_concs_vec[species_idx] = initial_concs[species[species_idx]]
+
+    _species = np.asarray(species)
+    s_idx = _species.argsort()
+    _species = list(_species[s_idx])
+    _react_stoch = react_stoch[:, s_idx]
+    _prod_stoch = prod_stoch[:, s_idx]
+    _stoch_mat = stoch_mat[:, s_idx]
+    _initial_concs_vec = initial_concs_vec[s_idx]
+
+    return (_species, reaction_rates, _react_stoch, _prod_stoch, _stoch_mat, number_species, number_reactions, _initial_concs_vec )
--- tests/test_crn_utils.py
+++ tests/test_crn_utils.py
@@ -17,8 +17,38 @@ def crn_to_save():
     csv_path = DATA_DIR / "crn_to_save.txt"
     return csv_path
 
+@pytest.fixture
+def test_crn():
+    TEST_DIR = Path(__file__).parent
+    DATA_DIR = TEST_DIR / "data"
+    csv_path = DATA_DIR / "test_crn.txt"
+    return csv_path
+
 def test_open_csv(csv_path):
     np.testing.assert_array_equal(utils.open_csv(csv_path), np.array([[1., 1.]]))
 
 def test_save_crn(crn_to_save):
     assert True
+
+def test_read_crn_txt(test_crn):
+
+    species, reaction_rates, react_stoch, prod_stoch, stoch_mat, number_species, number_reactions, initial_concs_vec = utils.read_crn_txt(test_crn)
+
+    _species = np.asarray(species)
+    assert np.isin('X_1', _species)
+    assert np.isin('Y_1', _species)
+    assert np.isin('X_2', _species)
+    assert not np.isin('Z_1', _species)
+    np.testing.assert_array_equal(_species, np.array(['X_1', 'X_2', 'Y_1']))
+    np.testing.assert_array_equal(reaction_rates, np.array([5.7, 10.3]))
+    np.testing.assert_array_equal(react_stoch, np.array([[1,0,1], [0,1,1]]))
+    np.testing.assert_array_equal(prod_stoch, np.array([[0,0,2], [0,0,1]]))
+    np.testing.assert_array_equal(stoch_mat, np.array([[-1,0,1], [0,-1,0]]))
+    assert number_species == 3
+    assert number_reactions == 2
+    np.testing.assert_array_equal(initial_concs_vec, np.array([9.0, 10.6, 11.0]))
+
+
+
+
+
